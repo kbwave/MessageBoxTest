49218fad301ad1e25ca7f5ccfe4ccff7f043d0d3
kbwave <kbwavemain@gmail.com>
2019-01-18 23:58:07 +0900

    Create Project
    Create the MessageBoxTest Project

---
+++ MessageBoxTest/Module/modMain.vb
@@ -0,0 +1,17 @@
+﻿''' <summary>
+''' 本アプリのメインモジュール
+''' </summary>
+''' <remarks></remarks>
+Module modMain
+
+	''' <summary>
+	''' エントリーポイント
+	''' </summary>
+	''' <remarks></remarks>
+	<STAThread> _
+	Public Sub Main()
+
+	End Sub
+
+
+End Module
